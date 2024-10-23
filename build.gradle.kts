94e3c6742451038ee50d94efd377320e15b425a9
srnyx <25808801+srnyx@users.noreply.github.com>
2024-10-23 18:01:29 -0400

    Add application main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 import xyz.srnyx.gradlegalaxy.enums.Repository
 import xyz.srnyx.gradlegalaxy.enums.repository
+import xyz.srnyx.gradlegalaxy.utility.setMainClass
 import xyz.srnyx.gradlegalaxy.utility.setupJava
 
 
 plugins {
+    application
     kotlin("jvm")
     id("xyz.srnyx.gradle-galaxy") version "1.3.2"
     id("io.github.goooler.shadow") version "8.1.8"
@@ -11,6 +13,7 @@ plugins {
 }
 
 setupJava("com.srnyx", "0.0.1", "A simple Discord user app")
+setMainClass("com.srnyx.auserapp.AUserApp")
 
 repository(Repository.MAVEN_CENTRAL, Repository.JITPACK)
 dependencies {
@@ -25,4 +28,12 @@ dependencies {
 
 kotlin {
     jvmToolchain(21)
+}
+
+// Fix some tasks
+tasks {
+    distZip { dependsOn("shadowJar") }
+    distTar { dependsOn("shadowJar") }
+    startScripts { dependsOn("shadowJar") }
+    startShadowScripts { dependsOn("jar") }
 }
