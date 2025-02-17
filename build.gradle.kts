f735f6c53d955e77cfea1f4b9484a87f2473dfa1
srnyx <25808801+srnyx@users.noreply.github.com>
2025-02-16 19:22:57 -0500

    Update dependencies
    Gradle: 8.12 -> 8.12.1
    Shadow: 8.3.5 -> 8.3.6
    JDA: fork (f5840dab04) -> main 5.3.0
    BotCommands: 09cddfe8a6 -> 3.0.0-alpha.24
    PostgreSQL: 42.7.4 -> 42.7.5
    HikariCP: 6.0.0 -> 6.2.1
    Logback Classic: 1.5.8 -> 1.5.16

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,26 @@
 import xyz.srnyx.gradlegalaxy.enums.Repository
 import xyz.srnyx.gradlegalaxy.enums.repository
-import xyz.srnyx.gradlegalaxy.utility.setMainClass
-import xyz.srnyx.gradlegalaxy.utility.setupJava
+import xyz.srnyx.gradlegalaxy.utility.setupJda
 
 
 plugins {
     application
     kotlin("jvm")
     id("xyz.srnyx.gradle-galaxy") version "1.3.3"
-    id("com.gradleup.shadow") version "8.3.5"
+    id("com.gradleup.shadow") version "8.3.6"
     id("dev.reformator.stacktracedecoroutinator") version "2.4.5"
 }
 
-setupJava("com.srnyx", "1.0.0", "A simple Discord user app")
-setMainClass("com.srnyx.auserapp.AUserApp")
+setupJda("5.3.0", "com.srnyx", "1.0.0", "A simple Discord user app")
 
 repository(Repository.MAVEN_CENTRAL, Repository.JITPACK)
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("io.github.JDA-Fork", "JDA", "f5840dab04")
-    implementation("io.github.freya022", "BotCommands", "09cddfe8a6")
+    implementation("io.github.freya022", "BotCommands", "3.0.0-alpha.24")
     implementation("org.spongepowered", "configurate-yaml", "4.1.2")
-    implementation("org.postgresql", "postgresql", "42.7.4")
-    implementation("com.zaxxer", "HikariCP", "6.0.0")
-    implementation("ch.qos.logback", "logback-classic", "1.5.8")
+    implementation("org.postgresql", "postgresql", "42.7.5")
+    implementation("com.zaxxer", "HikariCP", "6.2.1")
+    implementation("ch.qos.logback", "logback-classic", "1.5.16")
 }
 
 kotlin {
