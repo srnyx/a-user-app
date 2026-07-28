c69115f2c2ab980e4b0f2ca55d9832b971120dab
srnyx <25808801+srnyx@users.noreply.github.com>
2026-07-28 17:16:47 -0400

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import xyz.srnyx.gradlegalaxy.data.config.DependencyConfig
 import xyz.srnyx.gradlegalaxy.data.config.JavaSetupConfig
+import xyz.srnyx.gradlegalaxy.enums.repository
 import xyz.srnyx.gradlegalaxy.utility.setupLazyLibrary
 
 
@@ -8,14 +9,17 @@ plugins {
     application
     kotlin("jvm") version "2.4.0"
     id("xyz.srnyx.gradle-galaxy") version "3.2.0"
-    id("com.gradleup.shadow") version "9.5.1"
+    id("com.gradleup.shadow") version "9.6.1"
     id("dev.reformator.stacktracedecoroutinator") version "2.6.4"
 }
 
+repository("https://repo.freya02.dev/snapshots/")
 setupLazyLibrary(
-    javaSetupConfig = JavaSetupConfig("com.srnyx", "1.0.0", "A simple Discord user app"),
+    javaSetupConfig = JavaSetupConfig(
+        group = "com.srnyx",
+        description = "A simple Discord user app"),
     jdaConfig = DependencyConfig("6.5.0"),
-    lazyLibraryConfig = DependencyConfig("8cb5ea4"))
+    lazyLibraryConfig = DependencyConfig("8f35377"))
 
 dependencies {
     implementation(kotlin("stdlib"))
